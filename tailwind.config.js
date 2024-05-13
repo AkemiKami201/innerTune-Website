ba9140460d50b93fd3d12a06017bd3fe62bc4571
Chrono Lux <amber_c001@protonmail.com>
2024-05-12 21:16:33 -0600

    dark theme option

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ const flowbite = require("flowbite-react/tailwind");
 
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: ["class", "[data-theme=dark]"],
   content: [
     "./index.html", "./src/**/*.{js,ts,jsx,tsx}",
     flowbite.content(),
