1f1b60fd92bdba755c0576b4fa169d8394f969b3
Chrono Lux <amber_c001@protonmail.com>
2024-05-13 23:06:55 -0600

    color changes titles

--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -3,7 +3,7 @@ const MainSection = () => {
 		<div className="flex flex-col items-center mt-6 lg:mt-20 ">
 			<h1 className="text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide">
 				Inner
-			<span className="bg-gradient-to-r from-orange-500 to-red-800 text-transparent bg-clip-text">
+			<span className="bg-gradient-to-r from-rose-500 to-rose-900 text-transparent bg-clip-text">
 				Tune
 			</span>
 			<i class="fi fi-rr-waveform-path"></i>
@@ -12,7 +12,7 @@ const MainSection = () => {
 				Un cliente de YouTube Music con Material Design 3, para Android
 			</p>
 			<div className="flex justify-center my-10">
-				<a href="https://github.com/Arturo254/InnerTune/releases" className="bg-gradient-to-r from-orange-500 to-red-800 py-3 px-4 mx-3 rounded-md">Probar Gratis</a>
+				<a href="https://github.com/Arturo254/InnerTune/releases" className="bg-gradient-to-r from-rose-500 to-rose-900 py-3 px-4 mx-3 rounded-md">Probar Gratis</a>
 				<a href="https://github.com/Arturo254/InnerTune" className="py-3 px-4 mx-3 rounded-md border">Documentacion</a>
 			</div>
 		</div>
