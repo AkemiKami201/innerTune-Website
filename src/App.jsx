f7326f84c9fb75550fefc2035c2ef5f49c68a34f
Chrono Lux <amber_c001@protonmail.com>
2024-05-21 00:00:58 -0600

    small changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import FeatureSection from "./components/Features"
-import CarouselMD from "./components/Carousel";
+{/*import ActiveSlider from "./components/Carousel";*/}
 import MainSection from "./components/MainSection";
 import ChangeSection from "./components/ChangeSection";
 import FAQSection from "./components/FAQ";
@@ -14,8 +14,8 @@ const App = () => {
     <div className="max-w-7xl mx-auto pt-20 px-6">
       <MainSection />
       <hr class="my-12 h-0.5 border-t-0 bg-neutral-100 dark:bg-gray/10" />
-      <CarouselMD />
-      <hr class="my-12 h-0.5 border-t-0 bg-neutral-100 dark:bg-gray/10" />
+      {/*<ActiveSlider />
+      <hr class="my-12 h-0.5 border-t-0 bg-neutral-100 dark:bg-gray/10" />*/}
       <FeatureSection />
       <hr class="my-12 h-0.5 border-t-0 bg-neutral-100 dark:bg-gray/10" />
       <ChangeSection />
