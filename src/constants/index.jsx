486a1e5e728e1d20098d655c2b57a1b536375116
Chrono Lux <amber_c001@protonmail.com>
2024-05-23 23:52:40 -0600

    added carousel section

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -4,3 +4,59 @@ export const navItems = [
   { label: "Cambios y Soluciones", href: "#" },
   { label: "Preguntas frecuentes", href: "#" },
 ];
+
+import {
+  RxCrop,
+  RxDesktop,
+  RxPencil2,
+  RxReader,
+  RxRocket,
+  RxAccessibility,
+} from "react-icons/rx";
+
+import img1 from "../assets/media/img/1.jpg";
+import img2 from "../assets/media/img/2.jpg";
+import img3 from "../assets/media/img/3.jpg";
+import img4 from "../assets/media/img/4.jpg";
+import img5 from "../assets/media/img/5.jpg";
+import img6 from "../assets/media/img/6.jpg";
+import img7 from "../assets/media/img/7.jpg";
+
+export const ServiceData = [
+  {
+    icon: RxCrop,
+    title: "Development",
+    content: "Lorem ipsum dolor sit /amet, consectetur adipiscing elit.",
+    backgroundImage: img1,
+  },
+  {
+    icon: RxPencil2,
+    title: "Branding",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    backgroundImage: img2,
+  },
+  {
+    icon: RxDesktop,
+    title: "Design",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    backgroundImage: img3,
+  },
+  {
+    icon: RxReader,
+    title: "Seo",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    backgroundImage: img4,
+  },
+  {
+    icon: RxAccessibility,
+    title: "Management",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    backgroundImage: img5,
+  },
+  {
+    icon: RxRocket,
+    title: "Production",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    backgroundImage: img6,
+  },
+];
