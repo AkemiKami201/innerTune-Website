e5b4e9f17ec2d1c3b35c52c74e536f3fee15fbc4
Chrono Lux <amber_c001@protonmail.com>
2024-05-24 23:27:20 -0600

    deleted title, icon and content item

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -5,15 +5,6 @@ export const navItems = [
   { label: "Preguntas frecuentes", href: "#" },
 ];
 
-import {
-  RxCrop,
-  RxDesktop,
-  RxPencil2,
-  RxReader,
-  RxRocket,
-  RxAccessibility,
-} from "react-icons/rx";
-
 import img1 from "../assets/media/img/1.jpg";
 import img2 from "../assets/media/img/2.jpg";
 import img3 from "../assets/media/img/3.jpg";
@@ -21,42 +12,54 @@ import img4 from "../assets/media/img/4.jpg";
 import img5 from "../assets/media/img/5.jpg";
 import img6 from "../assets/media/img/6.jpg";
 import img7 from "../assets/media/img/7.jpg";
+import img8 from "../assets/media/img/8.jpg";
+import img9 from "../assets/media/img/9.jpg";
+import img10 from "../assets/media/img/10.jpg";
+import img11 from "../assets/media/img/11.jpg";
 
 export const ServiceData = [
   {
-    icon: RxCrop,
-    title: "Development",
-    content: "Lorem ipsum dolor sit /amet, consectetur adipiscing elit.",
+    title: "image 1",
     backgroundImage: img1,
   },
   {
-    icon: RxPencil2,
-    title: "Branding",
-    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    title: "image 2",
     backgroundImage: img2,
   },
   {
-    icon: RxDesktop,
-    title: "Design",
-    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    title: "image 3",
     backgroundImage: img3,
   },
   {
-    icon: RxReader,
-    title: "Seo",
-    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    title: "image 4",
     backgroundImage: img4,
   },
   {
-    icon: RxAccessibility,
-    title: "Management",
-    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    title: "image 5",
     backgroundImage: img5,
   },
   {
-    icon: RxRocket,
-    title: "Production",
-    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    title: "image 6",
     backgroundImage: img6,
+  },
+  {
+    title: "image 7",
+    backgroundImage: img7,
+  },
+  {
+    title: "image 8",
+    backgroundImage: img8,
+  },
+  {
+    title: "image 9",
+    backgroundImage: img9,
+  },
+  {
+    title: "image 10",
+    backgroundImage: img10,
+  },
+  {
+    title: "image 11",
+    backgroundImage: img11,
   },
 ];
