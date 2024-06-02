f7bebdca00a47e80109cf1ac616649194b387307
Chrono Lux <amber_c001@protonmail.com>
2024-06-01 21:29:35 -0600

    footer > new color, text white and added hover

--- src/components/FooterSection.jsx
+++ src/components/FooterSection.jsx
@@ -10,10 +10,11 @@ import {
 import { BsDribbble, BsFacebook, BsGithub, BsInstagram, BsTwitter } from "react-icons/bs";
 
 import logo from "../assets/waveform-path.svg";
+const year = new Date().getFullYear(); // get the current year
 
 const FooterSection = () => {
     return (
-	        <Footer className="bg-zinc-900" container>
+	        <Footer className="bg-gray-800" container>
 		      <div className="w-full">
 		        <div className="grid w-full justify-between sm:flex sm:justify-between md:flex md:grid-cols-1">
 		          	<div className="flex items-center flex-shrink-0 m-4 font-bold">
@@ -24,33 +25,33 @@ const FooterSection = () => {
 					</div>
 		          <div className="grid grid-cols-2 gap-8 sm:mt-4 sm:grid-cols-3 sm:gap-6">
 		            <div>
-		              <FooterTitle title="Acerca" />
+		              <FooterTitle className="text-white" title="Acerca" />
 		              <FooterLinkGroup col>
-		                <FooterLink href="https://github.com/Arturo254">Dev Aplicaci&oacute;n</FooterLink>
+		                <FooterLink className="text-neutral-400 hover:text-gray-200" href="https://github.com/Arturo254">Dev Aplicaci&oacute;n</FooterLink>
 		              </FooterLinkGroup>
 		            </div>
 		            <div>
-		              <FooterTitle title="Informaci&oacute;n" />
+		              <FooterTitle className="text-white" title="Informaci&oacute;n" />
 		              <FooterLinkGroup col>
-		                <FooterLink href="https://github.com/Arturo254/InnerTune">Github</FooterLink>
-		                <FooterLink href="https://raw.githubusercontent.com/Arturo254/InnerTune/master/LICENSE">Licencia</FooterLink>
+		                <FooterLink className="text-neutral-400 hover:text-gray-200" href="https://github.com/Arturo254/InnerTune">Github</FooterLink>
+		                <FooterLink className="text-neutral-400 hover:text-gray-200" href="https://raw.githubusercontent.com/Arturo254/InnerTune/master/LICENSE">Licencia</FooterLink>
 		              </FooterLinkGroup>
 		            </div>
 		            <div>
-		              <FooterTitle title="Soporte" />
+		              <FooterTitle className="text-white" title="Soporte" />
 		              <FooterLinkGroup col>
-		                <FooterLink href="https://github.com/Arturo254/InnerTune?tab=readme-ov-file#-c%C3%B3mo-reconstruir-la-aplicaci%C3%B3n">Contribuir</FooterLink>
-		                <FooterLink href="https://github.com/Arturo254/InnerTune/issues">Reportar bug</FooterLink>
+		                <FooterLink className="text-neutral-400 hover:text-gray-200" href="https://github.com/Arturo254/InnerTune?tab=readme-ov-file#-c%C3%B3mo-reconstruir-la-aplicaci%C3%B3n">Contribuir</FooterLink>
+		                <FooterLink className="text-neutral-400 hover:text-gray-200" href="https://github.com/Arturo254/InnerTune/issues">Reportar bug</FooterLink>
 		              </FooterLinkGroup>
 		            </div>
 		          </div>
 		        </div>
 		        <FooterDivider />
 		        <div className="w-full sm:flex sm:items-center sm:justify-between">
-		          <FooterCopyright href="https://github.com/Arturo254/InnerTune" by="InnerTune™" year={2024} />
+		          <FooterCopyright className="text-neutral-400 hover:text-gray-200" href="https://github.com/Arturo254/InnerTune" by="InnerTune™" year={year} />
 		          <FooterLinkGroup col className="p-1 items-center justify-between">
-		                <span className="text-1xl text-neutral-600">Aplicaci&oacute;n actualizada por <a className="font-bold tracking-tight text-gray-200 dark:text-gray" href="https:	//github.com/Arturo254">Arturo254</a></span>
-		                <span className="text-1xl text-neutral-600">Sitio mantenido por <a className="font-bold tracking-tight text-gray-200 dark:text-gray" href="https://github.com/ChronoLux616">ChronoLux</a></span>
+		                <span className="text-1xl text-neutral-400">Aplicaci&oacute;n actualizada por <a className="font-bold tracking-tight text-gray-300 hover:text-yellow-200" href="https:	//github.com/Arturo254">Arturo254</a></span>
+		                <span className="text-1xl text-neutral-400">Sitio mantenido por <a className="font-bold tracking-tight text-gray-300 hover:text-yellow-200" href="https://github.com/ChronoLux616">ChronoLux</a></span>
 		              </FooterLinkGroup>
 		          <div className="mt-4 flex space-x-6 sm:mt-0 sm:justify-center items-center justify-between">
 		            <FooterIcon href="#" icon={BsFacebook} />
