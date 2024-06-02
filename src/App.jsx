36c33e3071454849e00385d1341dfb1badc9ceed
Chrono Lux <amber_c001@protonmail.com>
2024-06-02 12:41:33 -0600

    alert component > added

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,14 @@ import FAQSection from "./components/FAQ";
 import DownloadSection from "./components/Download";
 import FooterSection from "./components/FooterSection";
 import Navbar from "./components/Navbar";
+import AlertUp from "./components/alertUpdate";
 
 const App = () => {
   return (
     <>
     <Navbar />
     <div className="max-w-7xl mx-auto pt-20 px-2">
+      <AlertUp />
       <MainSection />
       <hr class="my-12 h-0.5 border-t-0 bg-neutral-100 dark:bg-gray/10" />
       <ActiveSlider />
